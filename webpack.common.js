a4b23d35087df782a4e448cbb17c7d0cae7d128c
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2025-09-27 12:38:13 +0200

    Adding html and image loader

--- webpack.common.js
+++ webpack.common.js
@@ -19,6 +19,14 @@ module.exports = {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset/resource",
+            }
         ],
     },
 };
